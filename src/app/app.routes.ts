bb91650c83a5f91ff69f4d1e949db582c25dd587
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-11-29 11:58:55 -0300

    Criado pagina home com a rota e exemplo de tag "a"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AppointmentComponent } from './pages/appointment/appointment.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Personal Cleaner',
+    },
+    {
+        path: 'schedule-your-appointment',
+        component: AppointmentComponent,
+        title: 'Personal Cleaner',
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];
