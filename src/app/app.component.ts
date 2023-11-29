7c3940f8fb47cabcba67e981ccc8202759fec83a
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-11-29 11:57:56 -0300

    atualizado template padrão da página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavigationComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
