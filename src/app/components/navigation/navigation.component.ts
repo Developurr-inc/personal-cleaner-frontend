143bcece363229361b8440c605622da3c8bb04b3
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-12-04 20:03:26 -0300

    Created navigation

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,13 +1,51 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage, provideImgixLoader } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage, RouterLink],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.css'
+  styleUrl: './navigation.component.css',
+  providers: [
+    provideImgixLoader('http://localhost:4200/assets'),
+  ]
 })
 export class NavigationComponent {
+  routes = [
+    {
+      id: 1,
+      name: 'Home',
+      path: '/',
+      active: false
+    },
+    {
+      id: 2,
+      name: 'Sobre Nós',
+      path: '/about-us',
+      active: false
+    },
+    {
+      id: 3,
+      name: 'Contato',
+      path: '/contact-us',
+      active: false
+    },
+    {
+      id: 4,
+      name: 'Agendamento',
+      path: '/schedule-your-appointment',
+      active: false
+    }
+  ];
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event: any) => {
+      this.routes.forEach(route => {
+        route.active = this.router.url === route.path;
+      });
+    });
+  }
 }
