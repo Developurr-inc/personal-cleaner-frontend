140d88e4e21b86e288cb6eb096a8e8cc86f1d426
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2023-12-04 21:17:03 -0300

    feature, add navigation to expertise page and your html and css via tailWind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AppointmentComponent } from './pages/appointment/appointment.component';
+import { ExpertiseComponent } from './pages/expertise/expertise.component';
 
 export const routes: Routes = [
     {
@@ -13,6 +14,11 @@ export const routes: Routes = [
         path: 'schedule-your-appointment',
         component: AppointmentComponent,
         title: 'Personal Cleaner',
+    },
+    {
+        path: 'expertise-areas',
+        component: ExpertiseComponent,
+        title: 'Áreas de Atuação',
     },
     {
         path: '**',
