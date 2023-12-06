1d0d3c2476fcb1e34eb14a0eb533bbe846452d24
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-12-06 13:04:52 -0300

    criado rotas centralizadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,32 +4,34 @@ import { HomeComponent } from './pages/home/home.component';
 import { AppointmentComponent } from './pages/appointment/appointment.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { RouteUrlEnum } from './shared/enums/route-url.enum';
+import { RouteNameEnum } from './shared/enums/route-name.enum';
 
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent,
-        title: 'Personal Cleaner',
-    },
-    {
-        path: 'schedule-your-appointment',
-        component: AppointmentComponent,
-        title: 'Personal Cleaner',
-    },
-    {
-        path: 'contact-us',
-        component: ContactUsComponent,
-        title: 'Contact Us Page',
-    },
-    {
-        path: 'about-us',
-        component: AboutUsComponent,
-        title: 'About Us Page'
-    },
-    {
-        path: '**',
-        redirectTo: '',
-        pathMatch: 'full'
-    },
+  {
+    path: RouteUrlEnum.HOME,
+    component: HomeComponent,
+    title: `Personal Cleaner - ${RouteNameEnum.HOME}`,
+  },
+  {
+    path: RouteUrlEnum.APPOINTMENT,
+    component: AppointmentComponent,
+    title: `Personal Cleaner - ${RouteNameEnum.APPOINTMENT}`,
+  },
+  {
+    path: RouteUrlEnum.CONTACT_US,
+    component: ContactUsComponent,
+    title: `Personal Cleaner - ${RouteNameEnum.CONTACT_US}`,
+  },
+  {
+    path: RouteUrlEnum.ABOUT_US,
+    component: AboutUsComponent,
+    title: `Personal Cleaner - ${RouteNameEnum.ABOUT_US}`,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
 ];
