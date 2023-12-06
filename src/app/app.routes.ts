27ad12b2355c91dd8bd69a3670ecb397e0ea4987
GuilhermeKO <131626081+GuilhermeKO@users.noreply.github.com>
2023-12-06 14:24:47 -0300

    Criada tela de footer (#18)
    Signed-off-by: Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
    Co-authored-by: Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { AppointmentComponent } from './pages/appointment/appointment.component'
 import { ExpertiseComponent } from './pages/expertise/expertise.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
-
+import { FooterComponent } from './pages/footer/footer.component';
 import { RouteUrlEnum } from './shared/enums/route-url.enum';
 import { RouteNameEnum } from './shared/enums/route-name.enum';
 import { CtaComponent } from './pages/cta/cta.component';
 
 export const routes: Routes = [
+
   {
     path: RouteUrlEnum.HOME,
     component: HomeComponent,
@@ -40,5 +41,11 @@ export const routes: Routes = [
         path: 'cta',
         component: CtaComponent,
         title: 'CTA Page'
+
+    },
+    {
+        path: 'footer',
+        component: FooterComponent,
+        title: 'Footer Page'
     },
 ];
