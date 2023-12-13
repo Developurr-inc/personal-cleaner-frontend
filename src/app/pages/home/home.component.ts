0ac006862b4f4efd569ab582496cd99c10b17e75
Guilherme Kneitz <guikneitz@gmail.com>
2023-12-13 18:02:19 -0300

    Creating hero component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CtaComponent } from '../../shared/components/cta/cta.component';
+import { HeroComponent } from '../../shared/components/hero/hero.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CtaComponent],
+  imports: [CommonModule, CtaComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
