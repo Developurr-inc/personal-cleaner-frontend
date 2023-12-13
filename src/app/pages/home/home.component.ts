5137f85813445eb0c7df2c873c6b32e889e16158
Dudu Seabra <duduseabra99@gmail.com>
2023-12-13 18:02:45 -0300

    Create blog-sections component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CtaComponent } from '../../shared/components/cta/cta.component';
+import { BlogSectionsComponent } from '../../shared/components/blog-sections/blog-sections.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, CtaComponent],
+  imports: [CommonModule, CtaComponent, BlogSectionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
