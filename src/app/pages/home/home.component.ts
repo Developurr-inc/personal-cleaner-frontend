285fa1078e366eff2bfef0a12adc7955be8837ea
GuilhermeKO <131626081+GuilhermeKO@users.noreply.github.com>
2023-12-14 18:03:12 -0300

    Feature/create hero component (#50)
    * Creating hero component
    
    * Criada componente Hero
    
    * Mais alterações
    
    ---------
    
    Signed-off-by: Vinícius Ferreira Aguiar <acevinicius@icloud.com>
    Co-authored-by: Vinícius Ferreira Aguiar <acevinicius@icloud.com>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeroComponent } from '../../shared/components/hero/hero.component';
 import { SegmentComponent } from '../../shared/components/segment/segment.component';
-import { BadgeComponent } from '../../shared/components/badge/badge.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SegmentComponent, BadgeComponent],
+  imports: [CommonModule, SegmentComponent, HeroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
---
+++ src/app/shared/components/hero/hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage, RouterModule],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css',
+})
+export class HeroComponent {}
