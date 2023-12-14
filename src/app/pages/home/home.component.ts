fdf0f8c36e34f2214b64f7cdf710b23f1685839a
DuduSeabra <85571379+DuduSeabra@users.noreply.github.com>
2023-12-14 18:05:32 -0300

    Feature/create new component blog sections (#51)
    * Create blog-sections component
    
    * Create component blog-sections
    
    ---------
    
    Signed-off-by: Vinícius Ferreira Aguiar <acevinicius@icloud.com>
    Co-authored-by: Vinícius Ferreira Aguiar <acevinicius@icloud.com>

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BlogSectionsComponent } from '../../shared/components/blog-sections/blog-sections.component';
 import { HeroComponent } from '../../shared/components/hero/hero.component';
 import { SegmentComponent } from '../../shared/components/segment/segment.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SegmentComponent, HeroComponent],
+  imports: [CommonModule, SegmentComponent, HeroComponent, BlogSectionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
