df6d45137260b022d07f49e0aa9aa6f9246e08cb
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-12-14 02:10:45 -0300

    Created the badge functionality to the CTA

---
+++ src/app/shared/consts/segments.const.ts
@@ -0,0 +1,74 @@
+export const SEGMENTS: any[] = [
+  {
+    id: 1,
+    title: 'Restaurantes Comerciais e Fast Foods',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-yellow-500',
+  },
+  {
+    id: 2,
+    title: 'Concessionaria de Alimentação',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-blue-500',
+  },
+  {
+    id: 3,
+    title: 'Hotéis e Pousadas',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-indigo-500',
+  },
+  {
+    id: 4,
+    title: 'Hospitais, Clinicas e Laboratórios',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-green-500',
+  },
+  {
+    id: 5,
+    title: 'Indústrias',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-red-500',
+  },
+  {
+    id: 6,
+    title: 'Supermercados',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-purple-500',
+  },
+  {
+    id: 7,
+    title: 'Clubes e Academias',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-pink-500',
+  },
+  {
+    id: 8,
+    title: 'Escolas e Edifícios Comerciais',
+    subtitle: 'Subtitle',
+    content: 'Start your journey with us',
+    image:
+      'https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply',
+    color: 'bg-fuchsia-500',
+  },
+];
