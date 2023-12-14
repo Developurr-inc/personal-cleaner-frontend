946a12e07473af9170c8bb945c7758e18cfce894
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-12-14 02:14:54 -0300

    Feature/badge (#45)
    * Created the badge functionality to the CTA
    
    * Minor fixes

--- src/app/shared/components/cta/cta.component.ts
+++ src/app/shared/components/cta/cta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -9,4 +9,10 @@ import { RouterModule } from '@angular/router';
   templateUrl: './cta.component.html',
   styleUrl: './cta.component.css',
 })
-export class CtaComponent {}
+export class CtaComponent {
+  @Input({ required: true }) title!: string;
+  @Input({ required: true }) content!: string;
+  @Input({ required: true }) image!: string;
+  @Input() subtitle: string | null = null;
+  @Input() color: string = 'text-indigo-600';
+}
