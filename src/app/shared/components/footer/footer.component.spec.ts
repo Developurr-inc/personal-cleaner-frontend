78e322c7ba3feb078a5305037e73f09844ddaa47
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2023-12-15 01:37:32 -0300

    Fix/broken tests (#54)
    * shore
    
    * shore
    
    * fixed tests

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { FooterComponent } from './footer.component';
+
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -8,7 +10,7 @@ describe('FooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent],
+      imports: [FooterComponent, RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
