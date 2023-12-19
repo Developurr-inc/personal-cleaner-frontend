4403ca08657793cd3974627619210df25f14d2ad
DuduSeabra <85571379+DuduSeabra@users.noreply.github.com>
2023-12-18 21:48:58 -0300

    Criado pagina not found (#57)
    * Criado pagina not found
    
    * Fixed tests
    
    ---------
    
    Co-authored-by: Vinícius Ferreira Aguiar <acevinicius@icloud.com>

--- src/app/pages/notfound/notfound.component.spec.ts
+++ src/app/pages/notfound/notfound.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { FooterComponent } from './footer.component';
+import { NotfoundComponent } from './notfound.component';
 
-describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
+describe('NotfoundComponent', () => {
+  let component: NotfoundComponent;
+  let fixture: ComponentFixture<NotfoundComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent, RouterTestingModule],
+      imports: [NotfoundComponent, RouterTestingModule],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(FooterComponent);
+    fixture = TestBed.createComponent(NotfoundComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
