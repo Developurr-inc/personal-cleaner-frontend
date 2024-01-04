b32af9c350b0cf39c4c0cc06d6281581bb1711e0
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2024-01-04 13:27:04 -0300

    add, arquivos base para essa refactor

---
+++ src/app/shared/models/paper-products.model.ts
@@ -0,0 +1,7 @@
+export type PaperPModel = {
+  id: number;
+  title: string;
+  subtitle: string;
+  price: string;
+  image: string;
+};
---
+++ src/app/shared/models/support-products.model.ts
@@ -0,0 +1,7 @@
+export type SupportPModel = {
+  id: number;
+  title: string;
+  subtitle: string;
+  price: string;
+  image: string;
+};
