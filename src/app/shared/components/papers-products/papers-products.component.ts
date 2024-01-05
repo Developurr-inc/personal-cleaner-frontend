ca842712f18f9991d9c3eda92b3e9abe341a4e73
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2024-01-05 16:25:45 -0300

    Refactoring Products page to become more automatized (#98)
    * new update of pakage-lock.json
    
    * add the archives needed for create a for function
    
    * add, arquivos base para essa refactor
    
    * rethought the way that i would perferm the task so I deleted the unecessary archives
    
    * Was created the automatization of the papers section refer to the products page
    
    * was add the automatization of the products page refering to the section of support materials
    
    * runned prettier
    
    ---------
    
    Signed-off-by: Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>

---
+++ src/app/shared/components/papers-products/papers-products.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { PAPERS_PRODUCTS } from '../../consts/paper-products.const';
+
+@Component({
+  selector: 'app-papers-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './papers-products.component.html',
+  styleUrl: './papers-products.component.css',
+})
+export class PapersProductsComponent {
+  papers = PAPERS_PRODUCTS;
+}
