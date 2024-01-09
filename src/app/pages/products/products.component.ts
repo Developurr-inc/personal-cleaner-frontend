d9e6490388cd5bf80992dea1a0f0798887deb7ac
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2024-01-09 15:59:26 -0300

    add navigation function between components

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 import { PapersProductsComponent } from '../../shared/components/papers-products/papers-products.component';
 import { SupportProductsComponent } from '../../shared/components/support-products/support-products.component';
-import { ChemicalsComponent } from '../../shared/components/chemicals/chemicals.component'
+import { ChemicalsComponent } from '../../shared/components/chemicals/chemicals.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [PapersProductsComponent, SupportProductsComponent,ChemicalsComponent],
+  imports: [
+    PapersProductsComponent,
+    SupportProductsComponent,
+    ChemicalsComponent,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
 export class ProductsComponent {
-  btnChemicals = document.getElementById('btn-chemicals');
+  apperComponent() {
+    const productContent: HTMLElement | null =
+      document.getElementById('products-content');
+    const chemicalsContent: HTMLElement | null =
+      document.getElementById('chemicals-content');
+    const btnProducts = document.getElementById('btn-products');
+    const btnChemicals = document.getElementById('btn-chemicals');
+
+    if (productContent?.classList.contains('hidden')) {
+      productContent?.classList.remove('hidden');
+      chemicalsContent?.classList.add('hidden');
+      btnProducts?.classList.add('bg-indigo-600');
+      btnChemicals?.classList.remove('bg-indigo-600');
+    } else {
+      productContent?.classList.add('hidden');
+      chemicalsContent?.classList.remove('hidden');
+      btnProducts?.classList.remove('bg-indigo-600');
+      btnChemicals?.classList.add('bg-indigo-600');
+    }
+  }
 }
