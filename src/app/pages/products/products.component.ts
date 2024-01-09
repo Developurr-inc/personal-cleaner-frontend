38ed47b3a782dd54204631f36b99a645c0e483ff
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2024-01-09 12:14:51 -0300

    ainda analizado como resolver essa feature

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { PapersProductsComponent } from '../../shared/components/papers-products/papers-products.component';
 import { SupportProductsComponent } from '../../shared/components/support-products/support-products.component';
+import { ChemicalsComponent } from '../../shared/components/chemicals/chemicals.component'
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [PapersProductsComponent, SupportProductsComponent],
+  imports: [PapersProductsComponent, SupportProductsComponent,ChemicalsComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  btnChemicals = document.getElementById('btn-chemicals');
+}
