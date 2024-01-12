fde593d5cba5e1dd5364537f67617ae72bf69f19
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2024-01-12 14:11:04 -0300

    Feature/switchproducts e chemicals (#129)
    * ainda analizado como resolver essa feature
    
    * add navigation function between components
    
    * fix the color of the text when with white theme
    
    * add commit on products.component.ts
    
    * add commit on products.component.html
    
    ---------
    
    Signed-off-by: Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
 import { PapersProductsComponent } from '../../shared/components/papers-products/papers-products.component';
 import { SupportProductsComponent } from '../../shared/components/support-products/support-products.component';
+import { ChemicalsComponent } from '../../shared/components/chemicals/chemicals.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [PapersProductsComponent, SupportProductsComponent],
+  imports: [
+    PapersProductsComponent,
+    SupportProductsComponent,
+    ChemicalsComponent,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  apperComponent() {//Essa função altera as cores dos buttons e seus textos também além disso já está otimizada para o tema claro também
+    const productContent: HTMLElement | null =
+      document.getElementById('products-content');
+    const chemicalsContent: HTMLElement | null =
+      document.getElementById('chemicals-content');
+    const btnProducts = document.getElementById('btn-products');
+    const btnChemicals = document.getElementById('btn-chemicals');
+
+    if (productContent?.classList.contains('hidden')) {
+      productContent?.classList.remove('hidden');
+      chemicalsContent?.classList.add('hidden');
+      btnProducts?.classList.add('bg-indigo-600');
+      btnProducts?.classList.add('text-neutral-50');
+      btnChemicals?.classList.remove('bg-indigo-600');
+      btnChemicals?.classList.remove('text-neutral-50');
+    } else {
+      productContent?.classList.add('hidden');
+      chemicalsContent?.classList.remove('hidden');
+      btnProducts?.classList.remove('bg-indigo-600');
+      btnProducts?.classList.remove('text-neutral-50');
+      btnChemicals?.classList.add('bg-indigo-600');
+      btnChemicals?.classList.add('text-neutral-50');
+    }
+  }
+}
