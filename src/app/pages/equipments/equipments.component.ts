74102c6ac30708b1614c643a582b80c5dbbdc03b
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2024-01-17 15:10:57 -0300

    add the text of the training, technical and equipment components and liked with your repective pages, also exclude the previous conection with the content component. This last was also exclude due to be unecessary

--- src/app/pages/equipments/equipments.component.ts
+++ src/app/pages/equipments/equipments.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { ContentComponent } from '../../shared/components/content/content.component';
+
+import { EquipmentContentComponent } from '../../shared/components/equipment-content/equipment-content.component';
 import { StatsComponent } from '../../shared/components/stats/stats.component';
 
 @Component({
   selector: 'app-equipments',
   standalone: true,
-  imports: [ContentComponent, StatsComponent],
+  imports: [EquipmentContentComponent, StatsComponent],
   templateUrl: './equipments.component.html',
   styleUrl: './equipments.component.css',
 })
---
+++ src/app/shared/components/equipment-content/equipment-content.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EquipmentContentComponent } from './equipment-content.component';
+
+describe('EquipmentContentComponent', () => {
+  let component: EquipmentContentComponent;
+  let fixture: ComponentFixture<EquipmentContentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EquipmentContentComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(EquipmentContentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
