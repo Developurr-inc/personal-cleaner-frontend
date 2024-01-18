2238d88d3027a8c57be5c7d8de2b7b7432bbc62d
Vinícius Ferreira Aguiar <acevinicius@icloud.com>
2024-01-18 00:52:57 -0300

    Carregando url da variavel de ambiente, para funcionar no servidor corretamente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
     {
       provide: IMAGE_LOADER,
       useValue: (config: ImageLoaderConfig) => {
-        let url = `http://localhost:4200`;
+        let url = process.env['BASE_URL'] ?? 'http://localhost:4200';
 
         if (!config.src.endsWith('.webp')) {
           return url + config.src;
