fa8099570220a4828826d674047a60516ff5f689
Marcello Cavalcanti <127243557+MarcelloSCavalcanti@users.noreply.github.com>
2024-02-25 02:58:31 -0300

    fix faqs to portugues title and add image to quimicos

--- src/app/shared/components/chemicals/chemicals.component.ts
+++ src/app/shared/components/chemicals/chemicals.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-chemicals',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, NgOptimizedImage],
   templateUrl: './chemicals.component.html',
   styleUrl: './chemicals.component.css',
 })
